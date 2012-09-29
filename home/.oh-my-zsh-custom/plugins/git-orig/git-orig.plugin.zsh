37fa74dd08b40dd4df16e667610d1cc0d2036718
ama-ch <amach97@gmail.com>
2012-09-29 18:35:25 +0900

    add compdef settings.

--- home/.oh-my-zsh-custom/plugins/git-orig/git-orig.plugin.zsh
+++ home/.oh-my-zsh-custom/plugins/git-orig/git-orig.plugin.zsh
@@ -1,11 +1,20 @@
-# alias
+# Aliases
 alias gs='git s'
+compdef _git gs=git-status
 alias gl='git l'
+compdef _git gl=git-log
 alias glp='git lp'
+compdef _git glp=git-log
 alias gd='git d'
+compdef _git gd=git-diff
 alias gdc='git dc'
+compdef _git gdc=git-diff
 alias gsh='git stash'
+compdef _git gsh=git-stash
 alias gsp='git stash pop'
+compdef _git gsp=git-stash
 alias gsl='git stash list'
+compdef _git gsl=git-stash
 alias gpr='git pr'
+compdef _git gpr=git-pull
 alias gw='git web'
