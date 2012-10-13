4b7512a3053621c5e0e841ddf4f13649bc7f93ad
ama-ch <amach97@gmail.com>
2012-10-13 15:00:25 +0900

    Split function to filter arguments and execute commands.

--- home/.oh-my-zsh-custom/plugins/git-orig/git-orig.plugin.zsh
+++ home/.oh-my-zsh-custom/plugins/git-orig/git-orig.plugin.zsh
@@ -27,39 +27,46 @@ function is_number() {
   echo $1 | egrep -q '^[0-9]+$'
 }
 
+function is_not_number() {
+  echo $1 | egrep -qv '^[0-9]+$'
+}
+
 function get_nth_changed_file() {
   git status -sb | grep -v "^#" | head -n $1 | tail -n 1 | awk '{print $NF}'
 }
 
-function process_gitargs() {
-  local command=$1; shift
-  local nums=""
-  local args=""
-  for arg in $@; do
-    if is_number $arg; then
-      nums="$nums $arg"
-    else
-      args="$args $arg"
+function filter_arguments() {
+  local func=$1; shift
+  local ret=""
+  for arg in "$@"; do
+    if $func $arg; then
+      ret="$ret $arg"
     fi
   done
+  echo $ret
+}
 
-  for num in $(echo ${nums:-1}); do
+function call_command_with_args() {
+  local command=$1; shift
+  local num_args="$(filter_arguments is_number $@)"
+  local other_args="$(filter_arguments is_not_number $@)"
+  for num in $(echo ${num_args:-1}); do
     local file=$(get_nth_changed_file ${num})
-    git $command $(echo $args) $file
+    $(echo $command) $(echo $other_args) $file
   done
 }
 
 function add_nth_file() {
-  process_gitargs add $@ && \
+  call_command_with_args "git add" $@ && \
   git status -sb
 }
 
 function diff_nth_file() {
-  process_gitargs diff $@
+  call_command_with_args "git diff" $@
 }
 
 function checkout_nth_file() {
-  process_gitargs checkout $@ && \
+  call_command_with_args "git checkout" $@ && \
   git status -sb
 }
 
