e1bd10891e561167d020133acda7e1be926b73d2
ama-ch <amach97@gmail.com>
2014-07-09 20:36:43 +0900

    Change GOPATH to HOME.

--- home/.oh-my-zsh-custom/plugins/golang-path/golang-path.plugin.zsh
+++ home/.oh-my-zsh-custom/plugins/golang-path/golang-path.plugin.zsh
@@ -1,8 +1,5 @@
 if [[ -d /usr/local/go ]]; then
   export PATH=$PATH:/usr/local/go/bin
-  if [[ ! -d $HOME/go ]]; then
-    mkdir $HOME/go
-  fi
-  export GOPATH=$HOME/go
+  export GOPATH=$HOME
   export PATH=$PATH:$GOPATH/bin
 fi
