05f605bd8d8a6bb7eceadfb7cf870537806dca47
ama-ch <amach97@gmail.com>
2014-07-28 06:31:59 +0900

    Add peco function to select recent committed branch.

--- home/.oh-my-zsh-custom/plugins/peco/peco.plugin.zsh
+++ home/.oh-my-zsh-custom/plugins/peco/peco.plugin.zsh
@@ -60,6 +60,32 @@ function peco-cdr () {
 zle -N peco-cdr
 bindkey '^[' peco-cdr
 
+function peco-git-recent-branches () {
+    local selected_branch=$(git for-each-ref --format='%(refname)' --sort=-committerdate refs/heads | \
+        perl -pne 's{^refs/heads/}{}' | \
+        peco)
+    if [ -n "$selected_branch" ]; then
+        BUFFER="git checkout ${selected_branch}"
+        zle accept-line
+    fi
+    zle clear-screen
+}
+zle -N peco-git-recent-branches
+bindkey '^x^b' peco-git-recent-branches
+
+function peco-git-recent-all-branches () {
+    local selected_branch=$(git for-each-ref --format='%(refname)' --sort=-committerdate refs/heads refs/remotes | \
+        perl -pne 's{^refs/(heads|remotes)/}{}' | \
+        peco)
+    if [ -n "$selected_branch" ]; then
+        BUFFER="git checkout -t ${selected_branch}"
+        zle accept-line
+    fi
+    zle clear-screen
+}
+zle -N peco-git-recent-all-branches
+bindkey '^xb' peco-git-recent-all-branches
+
 # list all functions start with "peco-"
 function peco-function-list () {
   local selected=$(functions | grep "^.*\ ()\ {" | sed -e "s| () {||" | grep peco- | grep -v function-list | peco --query "$LBUFFER")
