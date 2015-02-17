5ba9c7c4111eb15b1d4d75a72d50143aa6aef7d6
ama-ch <amach97@gmail.com>
2015-02-17 13:17:13 +0900

    Update ubuntu.plugin.zsh

--- home/.oh-my-zsh-custom/plugins/ubuntu/ubuntu.plugin.zsh
+++ home/.oh-my-zsh-custom/plugins/ubuntu/ubuntu.plugin.zsh
@@ -2,7 +2,7 @@ os=$(uname -s)
 if [[ "$os" == "Linux" ]]; then
   # exports
   export GDK_NATIVE_WINDOWS=1
-  JAVA_HOME=/usr/lib/jvm/default-java
+  JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
   MYSQL_HOME=/usr/local/mysql
   PATH=$PATH:$JAVA_HOME/bin:$MYSQL_HOME/bin
   #LD_LIBRARY_PATH=/usr/local/mysql-5.5/lib/mysql:$LD_LIBRARY_PATH
